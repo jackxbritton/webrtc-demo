0fc7d241c1726388948f135ac907886f589b9732
jack <jack.britton@maine.edu>
2017-07-21 23:20:03 -0400

    Bootstrap and a much cleaner look.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ connection.ondatachannel = function(event) {
     receive_channel = event.channel;
     //receive_channel.onmessage = event => console.log("receive_channel.onmessage - " + event.data);
     receive_channel.onmessage = function(event) {
-        document.getElementById("o-received").value = event.data;
+        $("#o-received").val(event.data);
     }
     receive_channel.onopen = () => console.log("receive_channel.onopen");
     receive_channel.onclose = () => console.log("receive_channel.onclose");
@@ -21,46 +21,49 @@ connection.ondatachannel = function(event) {
 connection.onicecandidate = function(event) {
     if (!event.candidate)
         return;
-    document.getElementById("o-ice").value = JSON.stringify(event.candidate);
+    // JSON-encode event.candidate and add to list.
+    var json = JSON.stringify(event.candidate);
+    var element = $("<li class='list-group-item'>" + json + "</li>");
+    $("#ice-candidates").append(element);
 }
 
 // Click to generate an offer.
-document.getElementById("b-generate-offer").addEventListener("click", function() {
+$("#b-generate-offer").click(function() {
     connection.createOffer()
         .then(offer => connection.setLocalDescription(offer))
         .then(function() {
-            document.getElementById("o-offer").value = JSON.stringify(connection.localDescription);
+            $("#o-offer").val(JSON.stringify(connection.localDescription));
         })
         .catch(error => console.log(error));
-}, false);
+});
 
 // Accept the offer.
-document.getElementById("b-accept-offer").addEventListener("click", function() {
-    var offer = new RTCSessionDescription(JSON.parse(document.getElementById("i-offer").value));
+$("#b-accept-offer").click(function() {
+    var offer = new RTCSessionDescription(JSON.parse($("#i-offer").val()));
     connection.setRemoteDescription(offer)
         .then(() => connection.createAnswer())
         .then(function(answer) {
             return connection.setLocalDescription(answer);
         })
         .then(function() {
-            document.getElementById("o-answer").value = JSON.stringify(connection.localDescription);
+            $("#o-answer").val(JSON.stringify(connection.localDescription));
         }).catch(error => console.log(error));
-}, false);
+});
 
 // Accept the answer.
-document.getElementById("b-accept-answer").addEventListener("click", function() {
-    var answer = new RTCSessionDescription(JSON.parse(document.getElementById("i-answer").value));
+$("#b-accept-answer").click(function() {
+    var answer = new RTCSessionDescription(JSON.parse($("#i-answer").val()));
     connection.setRemoteDescription(answer);
-}, false);
+});
 
 // Click to accept the ice candidate.
-document.getElementById("b-accept-ice").addEventListener("click", function() {
-    var ice = new RTCIceCandidate(JSON.parse(document.getElementById("i-ice").value));
+$("#b-accept-ice").click(function() {
+    var ice = new RTCIceCandidate(JSON.parse($("#i-ice").val()));
     connection.addIceCandidate(ice);
-}, false);
+});
 
 // Click to send.
-document.getElementById("b-send").addEventListener("click", function() {
-    var message = document.getElementById("i-send").value;
+$("#b-send").click(function() {
+    var message = $("#i-send").val();
     send_channel.send(message);
-}, false);
+});
